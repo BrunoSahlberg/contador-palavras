9eda85407f18163a8eaba7efd806e84a87f9356d
gabrielfogaca <gabriel.fogaca@unijui.edu.br>
2023-03-19 21:26:41 -0300

    contador usando match

--- index.js
+++ index.js
@@ -2,23 +2,24 @@
 
 function contarPalavras() {
   // Obter o valor do campo de texto e da palavra a ser contada
-  const codigo = document.getElementById("codigo").value;
-  let palavrasCodigo = codigo.split("\n");
+  const codigo = document.getElementById('codigo').value;
+  let palavrasCodigo = codigo.split('\n');
   console.log(palavrasCodigo);
   var contEscreva = 0;
   var contLeia = 0;
   var contEscreval = 0;
 
   for (let i = 0; i < palavrasCodigo.length; i++) {
-    if (palavrasCodigo[i] == "Escreva") {
+    const str = palavrasCodigo[i];
+    if (str.match('escreva')) {
       contEscreva++;
     }
 
-    if (palavrasCodigo[i] == "Escreval") {
+    if (str.match('escreval')) {
       contEscreval++;
     }
 
-    if (palavrasCodigo[i] == "leia") {
+    if (str.match('leia')) {
       contLeia++;
     }
     printPalavras(contEscreva, contEscreval, contLeia);
@@ -26,8 +27,9 @@ function contarPalavras() {
 }
 
 function printPalavras(contEscreva, contEscreval, contLeia) {
-  document.getElementById("contador").innerHTML =
-    `<p> Escreva: ${contEscreva} <br>
+  document.getElementById(
+    'contador',
+  ).innerHTML = `<p> Escreva: ${contEscreva} <br>
          Escreval: ${contEscreval} <br>
          Leia: ${contLeia} <br>
   </p>`;
